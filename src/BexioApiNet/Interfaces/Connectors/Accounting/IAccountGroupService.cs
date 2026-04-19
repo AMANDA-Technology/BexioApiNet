6110d7b74cf6520211fbd217863170f77ebef777
developer-agent <developer-agent@amanda-technology.ch>
2026-04-19 10:03:13 +0000

    feat(accounting): add AccountGroupService for GET /2.0/account_groups (issue #35)
    Introduces a new AccountGroupService exposing the list endpoint for Bexio
    account groups, mirroring the existing AccountService pattern. DI wire-up
    (IBexioApiClient / BexioApiClient / AspNetCore registration) is intentionally
    omitted and left to sub-issue #49 to avoid merge conflicts.

---
+++ src/BexioApiNet/Interfaces/Connectors/Accounting/IAccountGroupService.cs
@@ -0,0 +1,46 @@
+/*
+MIT License
+
+Copyright (c) 2022 Philip Näf <philip.naef@amanda-technology.ch>
+Copyright (c) 2022 Manuel Gysin <manuel.gysin@amanda-technology.ch>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System.Runtime.InteropServices;
+using BexioApiNet.Abstractions.Models.Accounting.AccountGroups;
+using BexioApiNet.Abstractions.Models.Api;
+using BexioApiNet.Models;
+
+namespace BexioApiNet.Interfaces.Connectors.Accounting;
+
+/// <summary>
+/// Service for getting account group entries in accounting namespace. <see href="https://docs.bexio.com/#tag/Account-Groups">Account Groups</see>
+/// </summary>
+public interface IAccountGroupService
+{
+    /// <summary>
+    /// Get a list of account groups. <see href="https://docs.bexio.com/#tag/Account-Groups/operation/v2ListAccountGroups">v2 List Account Groups</see>
+    /// </summary>
+    /// <param name="queryParameterAccountGroup">Query parameter specific for account groups</param>
+    /// <param name="autoPage">Fetch all possible results</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns></returns>
+    public Task<ApiResult<List<AccountGroup>>> Get([Optional] QueryParameterAccountGroup? queryParameterAccountGroup, [Optional] bool autoPage, [Optional] CancellationToken cancellationToken);
+}
